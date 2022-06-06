e8bd42fef061073d11a676d505499fac7dfd2f17
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-06-06 20:18:07 +0900

    create websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+
+export enum ReadyState {
+  Connecting = 0,
+  Open = 1,
+  Closing = 2,
+  Closed = 3,
+}
 
 function App() {
   const [message, setMessage] = useState("");
+  const [dialog, setDialog] = useState<string[]>([]);
+  const wsRef = useRef<WebSocket>();
 
-  const submit = () => {
-    console.log(message);
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (message === "") return;
+    if (wsRef.current?.readyState === ReadyState.Open) {
+      wsRef.current?.send(message);
+      setDialog([...dialog, `send: ${message}`]);
+      setMessage("");
+    }
   };
+
+  useEffect(() => {
+    wsRef.current = new WebSocket("ws://localhost:8888/ws");
+    wsRef.current.addEventListener("open", () => {});
+    return () => {
+      wsRef.current?.addEventListener("close", () => {});
+    };
+  }, []);
+
+  wsRef.current?.addEventListener("message", (msg) => {
+    setDialog([...dialog, `receive: ${msg.data}`]);
+  });
 
   return (
     <div className="App">
-      <section>message : {message}</section>
-      <input
-        type="text"
-        name="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={submit}>submit</button>
+      <section>status : {wsRef.current?.readyState}</section>
+      <section>
+        {dialog.map((d, i) => (
+          <p key={i}>{d}</p>
+        ))}
+      </section>
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
